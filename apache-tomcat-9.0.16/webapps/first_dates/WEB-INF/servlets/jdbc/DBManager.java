9a1ffb52a54a2d03ed172d52080070e00d2000a0
100346250 <100346250@alumnos.uc3m.es>
2019-05-04 10:46:26 +0200

    DBManager

--- apache-tomcat-9.0.16/webapps/first_dates/WEB-INF/servlets/jdbc/DBManager.java
+++ apache-tomcat-9.0.16/webapps/first_dates/WEB-INF/servlets/jdbc/DBManager.java
@@ -78,4 +78,93 @@ public class DBManager implements AutoCloseable {
 
         return registered;
     }
+    /*
+    public int getStock(int bookId) throws SQLException {
+    	String query = "SELECT cantidad FROM Existencias WHERE libro =?";
+    	int cantidad = 0;
+    	try(PreparedStatement st = connection.prepareStatement(query)){
+  	    st.setInt(1,bookId);
+  	    ResultSet rs = st.executeQuery();
+  	    while(rs.next()){
+	        cantidad = rs.getInt("cantidad");
+	      }
+	    }
+      return cantidad;
+    }
+    */
+    public User getRecommendations(int userId) throws SQLException{
+      String query = "SELECT * FROM Preferences WHERE id = ?";
+
+      try(PreparedStatement st = connection.prepareStatement(query)){
+        st.setInt(1,userId);
+  	    ResultSet rs = st.executeQuery();
+        rs.next()
+        Preferences preferences = new Preferences();
+        preferences.setMinAge(rs.getInt("minAge"));
+        preferences.setMaxAge(rs.getInt("maxAge"));
+        preferences.sexPref(rs.getInt("sexPref"));
+      }
+      String query = "SELECT id, username, name, gender, birthdate, photo,(DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),'2012-2-29')+1), '%Y')) AS age FROM Users WHERE age > ? and age < ? and gender = ? ORDER BY RAND() LIMIT 20";
+
+      List<User> recommendations = new ArrayList<User>();
+      try(PreparedStatement st = connection.prepareStatement(query)){
+        st.setInt(1,preferences.getMinAge());
+        st.setInt(2,preferences.getMaxAge());
+        st.setInt(3,preferences.getSexPref());
+        while(rs.next()){
+          User recommendation = new User();
+          recommendation.setId(rs.getInt("id"));
+          recommendation.setUsername(rs.getString("username"));
+          recommendation.setName(rs.getString("name"));
+          recommendation.setGender(rs.getInt("gender"));
+          recommendation.setBirthdate(rs.getInt("birthdate"));
+
+          recommendations.add(recommendation);
+        }
+        return recommendations;
+      }
+      return null;
+    }
+
+    public List<DateMatch> getDateList(int userId)throws SQLException {
+      String query = "SELECT * FROM Dates WHERE (dateOneId or dateTwoId) = ?";
+      List<DateMatch> dates = new ArrayList<DateMatch>();
+      try(PreparedStatement st = connection.prepareStatement(query)){
+  	    st.setInt(1,userId);
+  	    ResultSet rs = st.executeQuery();
+
+
+  	    while(rs.next()){
+          DateMatch dateMatch = new Date();
+          dateMatch.setId(rs.getInt("id")):
+          dateMatch.setDateOneId(rs.getInt("dateOneId"));
+          dateMatch.setDateTwoId(rs.getInt("dateTwoId"));
+          dateMatch.setStatus(rs.getString("status"));
+          dateMatch.setDateRequest(rs.getArray("dateRequest"));
+          dateMatch.setDateResponse(rs.getArray("dateResponse"));
+          dateMatch.setHourRequest(rs.getArray("hourRequest"));
+          dateMatch.setHourResponse(rs.getArray("hourResponse"));
+
+          dates.add(dateMatch);
+	      }
+        return dates;
+	    }
+      return null;
+    }
+
+    public InputStream getImage(int id){
+        String query = "SELECT photo FROM Users WHERE id = ?";
+
+        try(PreparedStatement st = connection.prepareStatement(query)){
+    	    st.setInt(1,userId);
+    	    ResultSet rs = st.executeQuery();
+          InputStream in = new InputStream();
+
+  	      rs.next()
+          in = rs.getBinaryStream("photo");
+          return in;
+  	    }
+        return null;
+      }
+    }
 }
